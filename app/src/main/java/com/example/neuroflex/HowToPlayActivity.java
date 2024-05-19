3a472ac65b25522a08794f0e31734c6527327c4d
Vincent Yono <vincentyono@protonmail.com>
2024-05-19 13:36:25 +0700

    Add How to play instruction

--- app/src/main/java/com/example/neuroflex/HowToPlayActivity.java
+++ app/src/main/java/com/example/neuroflex/HowToPlayActivity.java
@@ -11,20 +11,18 @@ public class HowToPlayActivity extends AppCompatActivity {
     private TextView _howToPlayContent;
     private Button _finishButton;
 
-    private String _gameActivity;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_play);
-        this._gameActivity = getIntent().getStringExtra("GAME_ACTIVITY");
+        String gameActivity = getIntent().getStringExtra("GAME_ACTIVITY");
 
         this._howToPlayContent = findViewById(R.id.how_to_play_content);
         this._finishButton = findViewById(R.id.finish_button);
 
-        Log.d("TEST", this._gameActivity);
+        Log.d("TEST", gameActivity);
 
-        if(this._gameActivity == "MATH_PUZZLE") {
+        if(gameActivity.equals("MATH_PUZZLE")) {
             String s = "How to play:\n" +
                     "- Question Display: In the middle of the screen, you will see an arithmetic question (eg. 12 + 7)\n" +
                     "- Input Box: To the right of the question, there is an input box where you will type your answer\n" +
@@ -32,13 +30,19 @@ public class HowToPlayActivity extends AppCompatActivity {
                     "- Score: Your current score is shown at the top of the screen, updating with each correct answer.";
             this._howToPlayContent.setText(s);
         }
-        else if(this._gameActivity == "LANGUAGE_GAME") {
+        else if(gameActivity.equals("LANGUAGE_GAME")) {
             String s = "How to play:\n" +
-                    "- Question Display: ";
-            this._howToPlayContent.setText(s);
+                    "- Question Display: Question are displayed on the screen\n" +
+                    "- Multiple Choice: you are given a set of choices, please choose the most appropriate answer\n" +
+                    "- Timer: A timer is displayed, counting down the time you have to answer the question \n" +
+                    "Score: Your current score is shown at the top of the screen, updating with each correct answer";
+            _howToPlayContent.setText(s);
         }
         else {
-            String s = "";
+            String s = "How to play:\n" +
+                    "- Objective: the objective of the game is to pick the card with two similar fruits\n" +
+                    "- Timer: A timer is displayed, counting down the time you have to answer the question \n" +
+                    "Score: Your current score is shown at the top of the screen, updating with each correct answer";
             this._howToPlayContent.setText(s);
         }
 
