9fd22e87c9aa2b0ff7336bc1cd8367c3d4ddc107
kim <91533432+kimberlymazel@users.noreply.github.com>
2024-05-19 19:58:01 +0700

    removed sound switch

--- app/src/main/java/com/example/neuroflex/PauseGame.java
+++ app/src/main/java/com/example/neuroflex/PauseGame.java
@@ -15,7 +15,6 @@ public class PauseGame extends AppCompatActivity {
     private Button _restartButton;
     private Button _exitButton;
     private String _gameActivity;
-    private Switch _soundSwitch;
     private String _difficutly;
 
     @Override
@@ -29,7 +28,6 @@ public class PauseGame extends AppCompatActivity {
         this._howToPlayButton = findViewById(R.id.how_to_play_button);
         this._restartButton = findViewById(R.id.restart_button);
         this._exitButton = findViewById(R.id.exit_button);
-        this._soundSwitch = findViewById(R.id.sound_switch);
 
         this._resumeButton.setOnClickListener(new View.OnClickListener() {
             @Override
