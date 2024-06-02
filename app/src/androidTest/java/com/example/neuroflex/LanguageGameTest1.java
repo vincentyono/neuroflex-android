183769969f276fceea64005c9fdc1dc550172d3c
Vincent Yono <vincentyono@protonmail.com>
2024-06-02 16:20:02 +0700

    unittest

--- app/src/androidTest/java/com/example/neuroflex/LanguageGameTest1.java
+++ app/src/androidTest/java/com/example/neuroflex/LanguageGameTest1.java
@@ -1,14 +1,12 @@
 package com.example.neuroflex;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import static org.hamcrest.core.AllOf.allOf;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -21,17 +19,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class GameMenuTest1 {
+public class LanguageGameTest1 {
     @Rule
     public ActivityScenarioRule<Login> login =
             new ActivityScenarioRule<>(Login.class);
 
     @Test
-    public void onClickMathGame() {
+    public void onClickPause() {
 //        onView(withId(R.id.email)).perform(typeText("tom@email.com"));
 //        onView(withId(R.id.password)).perform(typeText("Abc123123"), closeSoftKeyboard());
 //        onView(withId(R.id.btn_login)).perform(click());
-//
+
 //        try {
 //            Thread.sleep(5000);
 //        } catch (InterruptedException e) {
@@ -46,7 +44,7 @@ public class GameMenuTest1 {
             throw new RuntimeException(e);
         }
 
-        onView(withId(R.id.imageViewGame)).perform(click());
+        onView(withId(R.id.imageViewGame3)).perform(click());
 
         try {
             Thread.sleep(1000);
@@ -54,42 +52,28 @@ public class GameMenuTest1 {
             throw new RuntimeException(e);
         }
 
-        onView(allOf(withText("Math Game"), isDisplayed()));
+        onView(withId(R.id.btnEasy)).perform(click());
+        onView(withId(R.id.btnPlayNow)).perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        onView(withId(R.id.pause_btn));
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        onView(allOf(withText("Game Paused"), isDisplayed()));
     }
 
     @Test
-    public void onClickLanguageGame() {
-//        onView(withId(R.id.email)).perform(typeText("tom@email.com"));
-//        onView(withId(R.id.password)).perform(typeText("Abc123123"), closeSoftKeyboard());
-//        onView(withId(R.id.btn_login)).perform(click());
-//
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-
-        onView(withId(R.id.bottom_game)).perform(click());
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        onView(withId(R.id.imageViewGame2)).perform(click());
-
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
-        onView(allOf(withText("Language Game"), isDisplayed()));
-    }
-
-    @Test
-    public void onClickMemoryGame() {
+    public void onClickHelp() {
         onView(withId(R.id.email)).perform(typeText("tom@email.com"));
         onView(withId(R.id.password)).perform(typeText("Abc123123"), closeSoftKeyboard());
         onView(withId(R.id.btn_login)).perform(click());
@@ -116,6 +100,30 @@ public class GameMenuTest1 {
             throw new RuntimeException(e);
         }
 
-        onView(allOf(withText("Memory Game"), isDisplayed()));
+        onView(withId(R.id.btnEasy)).perform(click());
+        onView(withId(R.id.btnPlayNow)).perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        onView(withId(R.id.help_btn)).perform(click());
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        onView(allOf(withText("How to Play"), isDisplayed()));
     }
+
 }
